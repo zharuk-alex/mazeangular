46db5a05f9bad5aec99acd30a8e9500867f8d4c8
unknown <zzharuk@gmail.com>
2022-04-20 13:02:22 +0300

    updated

--- server.js
+++ server.js
@@ -3,9 +3,14 @@ const path = require('path');
 
 const app = express();
 
+const PORT = 8080;
+
 app.use(express.static(__dirname + '/dist/mazeangular'));
-app.get('/*', function(req,res){
+app.get('/*', (req,res)=>{
     res.sendFile(path.join(__dirname + '/dist/mazeangular/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || PORT, ()=>{
+    console.log(`app started at ${8080} port`);
+    console.log(`http://localhost:${PORT}/`);
+  });
